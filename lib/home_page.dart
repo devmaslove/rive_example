52d714d34360c3b45f3d1bb69224c5ab0060b642
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-02-09 22:36:37 +0300

    Добавил кнопки с анимациями в нижний бар + логика выбранной

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,8 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:wstore/wstore.dart';
 
+class RiveButton {
+  final String title;
+  final String artboard;
+  final String stateMachineName;
+  SMIBool? input;
+  bool selected;
+
+  RiveButton({
+    required this.title,
+    required this.artboard,
+    required this.stateMachineName,
+    this.selected = false,
+  });
+}
+
 class HomePageStore extends WStore {
   int counter = 0;
+
+  final buttons = [
+    RiveButton(
+      title: 'Чат',
+      stateMachineName: 'CHAT_Interactivity',
+      artboard: 'CHAT',
+      selected: true,
+    ),
+    RiveButton(
+      title: 'Поиск',
+      stateMachineName: 'SEARCH_Interactivity',
+      artboard: 'SEARCH',
+    ),
+    RiveButton(
+      title: 'Таймер',
+      stateMachineName: 'TIMER_Interactivity',
+      artboard: 'TIMER',
+    ),
+    RiveButton(
+      title: 'Звонок',
+      stateMachineName: 'BELL_Interactivity',
+      artboard: 'BELL',
+    ),
+    RiveButton(
+      title: 'Пользователь',
+      stateMachineName: 'USER_Interactivity',
+      artboard: 'USER',
+    ),
+  ];
+
+  void setSelected(RiveButton button) {
+    setStore(() {
+      button.selected = true;
+      buttons
+          .where((btn) => btn != button)
+          .forEach((btn) => btn.selected = false);
+    });
+  }
 
   void incrementCounter() {
     setStore(() {
@@ -62,13 +115,70 @@ class HomePage extends WStoreWidget<HomePageStore> {
             borderRadius: BorderRadius.all(Radius.circular(12)),
           ),
           child: Row(
-            children: const [
-              SizedBox(
-                height: 36,
-                width: 36,
-                child: RiveAnimation.asset(
-                  'assets/rive/icons.riv',
-                  artboard: 'LIKE/STAR',
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ...store.buttons.map(
+                (button) => WStoreValueBuilder<HomePageStore, bool>(
+                  watch: (store) => button.selected,
+                  store: store,
+                  builder: (context, selected) {
+                    return GestureDetector(
+                      onTap: () {
+                        button.input?.change(true);
+                        Future.delayed(
+                          const Duration(
+                            milliseconds: 100,
+                          ),
+                          () => button.input?.change(false),
+                        );
+                        store.setSelected(button);
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            margin: const EdgeInsets.all(2),
+                            height: 4,
+                            width: selected ? 20 : 0,
+                            decoration: const BoxDecoration(
+                              color: Color(0xFF81B4FF),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(12),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 36,
+                            width: 36,
+                            child: Opacity(
+                              opacity: selected ? 1 : 0.5,
+                              child: RiveAnimation.asset(
+                                'assets/rive/icons.riv',
+                                artboard: button.artboard,
+                                stateMachines: [button.stateMachineName],
+                                onInit: (artboard) {
+                                  for (var controller
+                                      in artboard.animationControllers) {
+                                    if (controller is StateMachineController &&
+                                        controller.stateMachine.name ==
+                                            button.stateMachineName) {
+                                      button.input =
+                                          controller.findSMI<SMIBool>('active');
+                                    }
+                                  }
+                                  assert(
+                                    button.input != null,
+                                    'Can\'t find state machine ${button.stateMachineName}',
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
             ],
